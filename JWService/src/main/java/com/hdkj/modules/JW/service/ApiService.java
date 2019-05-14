b701301ef140dbab9837dae45a2abc98625c4e23
dw782841314 <782841314@qq.com>
2019-05-14 11:23:09 +0800

    删除

--- JWService/src/main/java/com/hdkj/modules/JW/service/ApiService.java
+++
@@ -1,7 +0,0 @@
-package com.hdkj.modules.JW.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface ApiService {
-    String query(String json) throws JsonProcessingException;
-}
--- JWService/src/main/java/com/hdkj/modules/sys/test.java
+++
@@ -1,8 +0,0 @@
-package com.hdkj.modules.sys;
-
-public class test {
-    public static void main(String[] args) {
-
-
-    }
-}
